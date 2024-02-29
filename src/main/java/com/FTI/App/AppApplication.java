1d3f5596f20950363d6b3e2041004f97d52fe149
Marcos Pantoja <marcospantoja21@outlook.com>
2024-02-29 14:19:04 -0800

    Succesfully connected to mongoDB and saved a test log to the database.

--- src/main/java/com/FTI/App/AppApplication.java
+++ src/main/java/com/FTI/App/AppApplication.java
@@ -1,13 +1,38 @@
 package com.FTI.App;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.FTI.App.models.User;
+import com.FTI.App.repositories.UserRepository;
+
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication implements CommandLineRunner{
+
+	private final UserRepository userRepository;
+
+	@Autowired
+	public AppApplication(UserRepository userRepository){
+		this.userRepository = userRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
-}
+	@Override
+	public void run(String... args) throws Exception{
+		
+		if(userRepository.findAll().isEmpty()){
+
+			userRepository.save(new User("Alice","Smith"));
+			userRepository.save(new User("Bob","Smith"));
+		}
+
+		for(User user : userRepository.findAll()){
+			System.out.println(user);
+		}
+	}
+ }
